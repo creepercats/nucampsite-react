ab3e5fa2abb9319fe38c6e141a04122fc443cd34
Cody Trent <CodyTrentDev@Gmail.com>
2021-11-17 20:33:31 -0500

    Adding to the Single Page App

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,56 +1,56 @@
-import React from "react";
-import { Card, CardImg, CardBody, CardText, CardTitle } from "reactstrap";
+import React from 'react';
+import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
 
 function RenderComments({ comments }) {
-  return (
-    <div className="col-md-5 m-1">
-      <h4>Comments</h4>
-      {comments.map((comment) => {
-        return (
-          <div>
-            <h6>{comment.text}</h6>
-            <p>
-              -- {comment.author},{" "}
-              {new Intl.DateTimeFormat("en-US", {
-                year: "numeric",
-                month: "short",
-                day: "2-digit",
-              }).format(new Date(Date.parse(comment.date)))}
-            </p>
-          </div>
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className="col-md-5 m-1">
+            <h4>Comments</h4>
+            {comments.map((comment) => {
+                return (
+                    <div>
+                        <h6>{comment.text}</h6>
+                        <p>
+                            -- {comment.author},{' '}
+                            {new Intl.DateTimeFormat('en-US', {
+                                year: 'numeric',
+                                month: 'short',
+                                day: '2-digit',
+                            }).format(new Date(Date.parse(comment.date)))}
+                        </p>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 function RenderCampsite({ campsite }) {
-  return (
-    <div className="col-md-5 m-1">
-      <Card>
-        <CardImg top src={campsite.image} alt={campsite.name} />
-        <CardBody>
-          <CardTitle>{campsite.name}</CardTitle>
-          <CardText>{campsite.description}</CardText>
-        </CardBody>
-      </Card>
-    </div>
-  );
+    return (
+        <div className="col-md-5 m-1">
+            <Card>
+                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardTitle>{campsite.name}</CardTitle>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
 }
 
 function CampsiteInfo(props) {
-  if (props.campsite != null) {
-    return (
-      <div className="container">
-        <div className="row">
-          <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.campsite.comments} />
-        </div>
-      </div>
-    );
-  } else {
-    return <div></div>;
-  }
+    if (props.campsite != null) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.campsite.comments} />
+                </div>
+            </div>
+        );
+    } else {
+        return <div></div>;
+    }
 }
 
 export default CampsiteInfo;
--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,68 +1,74 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
-  return (
-    <div>
-      <footer className="site-footer">
-        <div className="container">
-          <div className="row">
-            <div className="col-4 col-sm-2 offset-1">
-              <h5>Links</h5>
-              <ul className="list-unstyled">
-                <li>
-                  <Link to="/home">Home</Link>
-                </li>
-                <li>
-                  <Link to="/directory">Directory</Link>
-                </li>
-                <li>
-                  <Link to="/aboutus">About</Link>
-                </li>
-                <li>
-                  <Link to="/contactus">Contact</Link>
-                </li>
-              </ul>
-            </div>
-            <div className="col-6 col-sm-3 text-center">
-              <h5>Social</h5>
-              <a
-                href="http://instagram.com/"
-                className="btn btn-social-icon btn-instagram"
-              >
-                <i className="fa fa-instagram"></i>
-              </a>{" "}
-              <a
-                href="http://www.facebook.com/"
-                className="btn btn-social-icon btn-facebook"
-              >
-                <i className="fa fa-facebook"></i>
-              </a>{" "}
-              <a
-                href="http://twitter.com/"
-                className="btn btn-social-icon btn-twitter"
-              >
-                <i className="fa fa-twitter"></i>
-              </a>{" "}
-              <a
-                href="http://youtube.com/"
-                className="btn btn-social-icon btn-google"
-              >
-                <i className="fa fa-youtube"></i>
-              </a>
-            </div>
-            <div className="col-sm-4 text-center">
-              <a href="tel:+12065551234" className="btn btn-link">
-                <i className="fa fa-phone"> 1-206-555-1234</i>
-              </a>
-              <br />
-              <a href="mailto:notreal@notreal.co" className="btn btn-link">
-                <i className="fa fa-envelope-o"> campsites@nucamp.co</i>
-              </a>
-            </div>
-          </div>
+    return (
+        <div>
+            <footer className="site-footer">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-4 col-sm-2 offset-1">
+                            <h5>Links</h5>
+                            <ul className="list-unstyled">
+                                <li>
+                                    <Link to="/home">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/directory">Directory</Link>
+                                </li>
+                                <li>
+                                    <Link to="/aboutus">About</Link>
+                                </li>
+                                <li>
+                                    <Link to="/contactus">Contact</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-6 col-sm-3 text-center">
+                            <h5>Social</h5>
+                            <a
+                                href="http://instagram.com/"
+                                className="btn btn-social-icon btn-instagram"
+                            >
+                                <i className="fa fa-instagram"></i>
+                            </a>{' '}
+                            <a
+                                href="http://www.facebook.com/"
+                                className="btn btn-social-icon btn-facebook"
+                            >
+                                <i className="fa fa-facebook"></i>
+                            </a>{' '}
+                            <a
+                                href="http://twitter.com/"
+                                className="btn btn-social-icon btn-twitter"
+                            >
+                                <i className="fa fa-twitter"></i>
+                            </a>{' '}
+                            <a
+                                href="http://youtube.com/"
+                                className="btn btn-social-icon btn-google"
+                            >
+                                <i className="fa fa-youtube"></i>
+                            </a>
+                        </div>
+                        <div className="col-sm-4 text-center">
+                            <a href="tel:+12065551234" className="btn btn-link">
+                                <i className="fa fa-phone"> 1-206-555-1234</i>
+                            </a>
+                            <br />
+                            <a
+                                href="mailto:notreal@notreal.co"
+                                className="btn btn-link"
+                            >
+                                <i className="fa fa-envelope-o">
+                                    {' '}
+                                    campsites@nucamp.co
+                                </i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
-      </footer>
-    </div>
-  );
+    );
 }
